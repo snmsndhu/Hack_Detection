f27d93476e65e52bcd78f74f80a5a39f58f6d920
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-06-24 10:28:17 -0400

    reentrancy

--- contracts/vulnerableContracts/reentrancy.sol
+++ contracts/vulnerableContracts/reentrancy.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract reentrancy {
-    constructor() {
-        
+contract ReentrancyVulnerableContract {
+    mapping(address => uint256) public balances;
+
+  function withdraw() public {
+
+        uint256 balance = balances[msg.sender];
+
+        // Send ETH 
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Withdraw failed");
+
+        // Update Balance
+        balances[msg.sender] = 0;
     }
 }
